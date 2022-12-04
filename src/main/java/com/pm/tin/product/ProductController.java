9c0f10adf30514ecb590f9f81b3a1bd39badef96
Tran Quang Tin <tintran1024@gmail.com>
2022-12-04 22:48:38 +0700

    test project reactor

--- src/main/java/com/pm/tin/product/ProductController.java
+++ src/main/java/com/pm/tin/product/ProductController.java
@@ -7,6 +7,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/api/products")
@@ -15,11 +16,15 @@ import org.springframework.web.bind.annotation.*;
 class ProductController {
     private final ProductService service;
     private final ProductMapper mapper;
-
-    @PostMapping(value = "/create", produces = MediaType.APPLICATION_JSON_VALUE)
-    ResponseEntity<ProductDto> createProduct(@RequestBody ProductReq req) {
+    
+    @PostMapping(
+            value = "/create",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    Mono<ResponseEntity<ProductDto>> createProduct(@RequestBody ProductReq req) {
         Product pe = service.create(req);
-        return ResponseEntity.ok(mapper.toPDto(pe));
+//        return ResponseEntity.ok(mapper.toPDto)(pe);
+        return Mono.just(ResponseEntity.ok(mapper.toPDto(pe)));
     }
 
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
